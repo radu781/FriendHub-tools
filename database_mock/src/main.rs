5ba498394d7af60aed869ccb1b78ba11f4daa401
radu781 <poparadu501@gmail.com>
2023-07-19 22:17:12 +0300

    Added tables support

--- database_mock/src/main.rs
+++ database_mock/src/main.rs
@@ -1,11 +1,15 @@
 mod database;
 use std::error::Error;
 
-use database::connector::DBConnection;
-use tokio;
+use database::{connector::DBConnection, tables::Adjust};
+use fake::{Fake, Faker};
+
+use crate::database::tables::User;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    let a = DBConnection::new();
+    let mut db_connection = DBConnection::new().await;
+    let user = Faker.fake::<User>().adjust();
+    db_connection.insert(&user).await;
     Ok(())
 }
